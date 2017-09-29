02720aa54a87d01ad7c64b13284fdc8b5fcd8002
dkittlel1 <dkittle@loyalty.com>
2017-09-29 16:52:51 -0400

    fixed scalatest dependency, started test

--- src/main/scala/actor/db/DbActor.scala
+++ src/main/scala/actor/db/DbActor.scala
@@ -1,3 +1,8 @@
 package actor.db
 
-class DbActor {}
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+class DbActor extends Actor {
+  override def receive: Receive = ???
+}
--- src/test/scala/actor/db/DbActorSpec.scala
+++ src/test/scala/actor/db/DbActorSpec.scala
@@ -1,11 +1,21 @@
 package actor.db
 
-import akka.actor.ActorSystem
-import org.scalatest.{FlatSpec, FlatSpecLike, Matchers}
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.TestKit
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, FlatSpecLike, Matchers}
 
-class DbActorSpec extends TestKit(ActorSystem("test-system")) with FlatSpecLike with Matchers {
+class DbActorSpec()
+    extends TestKit(ActorSystem("test-system"))
+    with FlatSpecLike
+    with Matchers
+    with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    TestKit.shutdownActorSystem(system)
+  }
 
   "a db actor" should "store a value" in {
-    assert(true)
+    val db = system.actorOf(Props[DbActor])
   }
 }

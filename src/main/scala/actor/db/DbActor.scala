07da4baddae5da2cdd27a397446b411f22199262
dkittlel1 <dkittle@loyalty.com>
2017-10-03 12:28:59 -0400

    chapter 2 solution

--- src/main/scala/actor/db/DbActor.scala
+++ src/main/scala/actor/db/DbActor.scala
@@ -7,8 +7,10 @@ import scala.collection.mutable
 
 class DbActor extends Actor with ActorLogging {
   val db = new mutable.HashMap[String, Object]
+
   override def receive: Receive = {
     case (k: String, v: Object) => db.put(k, v)
+    case k: String => sender() ! db.getOrElse(k, "")
     case o => log.info("unknown message ", o)
   }
 
